f19190e84ffb2ca0367d693f4bb1252bd372783d
Jonathan Roth <jonathan_roth@brown.edu>
2024-01-02 18:57:27 -0500

    updated lw app to do both men and women

--- lw-application.R
+++ lw-application.R
@@ -7,13 +7,17 @@ source(here('create_V_AR1.R'))
 source(here('eb_params_ar1.R'))
 
 #Load Benzarti Carloni results
-lw_women <- readRDS(here("Data/EmploymentFemale_RefPeriodMinusTwo.rds"))
 
+lw_women <- readRDS(here("Data/EmploymentFemale_RefPeriodMinusTwo.rds"))
+lw_men <- readRDS(here("Data/EmploymentMale_RefPeriodMinusTwo.rds"))
+
+lw_results_fn <-
+function(lw_object){
 #Multiply beta and Sigma to convert to pp (i.e. beta * 100,  sigma * 100^2)
-beta <- lw_women$beta * 100
-sigma <- lw_women$sigma * 100^2
-tVec <- lw_women$tVec
-referencePeriod <- lw_women$referencePeriod
+beta <- lw_object$beta * 100
+sigma <- lw_object$sigma * 100^2
+tVec <- lw_object$tVec
+referencePeriod <- lw_object$referencePeriod
 prePeriodIndices <- which(tVec < referencePeriod)
 
 w <- calc_w(beta = beta,
@@ -25,3 +29,30 @@ SigmaW <- calc_w(beta = beta,
             prePeriodIndices = prePeriodIndices)$SigmaW
 eb_params <- eb_params_ar1(w = w,
                            SigmaW = SigmaW)
+
+V_delta_EB <- 
+create_V_AR1(rho = 1,
+             sigma = sqrt(eb_params$sigmasq),
+             tVec = tVec,
+             referencePeriod = referencePeriod)
+
+mu_delta_EB <- eb_params$mu * (tVec - referencePeriod)
+
+result <-
+taupost_posterior(beta = beta,
+                  Sigma = sigma,
+                  Vdelta = V_delta_EB,
+                  mudelta = mu_delta_EB,
+                  tVec = tVec, 
+                  Vtaupost = NULL,
+                  referencePeriod = referencePeriod)
+
+return(result)
+
+}
+
+lw_results_fn(lw_men)$eventPlot +
+  xlim(-10,10) + ylim(-7,3)
+
+lw_results_fn(lw_women)$eventPlot +
+  xlim(-10,10) + ylim(-3,6)
